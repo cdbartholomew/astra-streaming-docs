c87d79133288c0e22bac2160b668b87deb68af8e
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2023-03-22 11:15:36 -0400

    allowed

---
+++ modules/ROOT/examples/allowed_georep.sh
@@ -0,0 +1,5 @@
+curl --location --head '{base_url}{base_api}/tenants/test-llp' \
+--header "Authorization: Bearer {bearer_token}"
+
+
+http://astra-streaming-admin.docs.streaming.datastax.com/v2/streaming/replications-peers/{instance}/{cluster}
